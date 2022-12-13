cbd9533e564e59b9df336c7423da9b0cf380a095
Nick Gatti <nick.gatti@gmail.com>
2022-12-13 16:02:35 -0700

    ws server

--- server/server.js
+++ server/server.js
@@ -1,40 +1,10 @@
 const express = require('express')
-const session = require('express-session')
 const path = require('path')
 
 const app = express()
 const port = 3000
 
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true
-  }))
-
-const players = {
-    playerOne: null,
-    playerTwo: null,
-}
-
 app.use('/', express.static(path.join(__dirname, '../../stickfigurefightingfrontend/build')))
-
-app.get('/player', (req, res) => {
-    if (!players.playerOne) {
-        players.playerOne = req.session.id
-        req.session.player = 'Player one'
-        res.json({
-            player: 1,
-        })
-        console.log('Player One has joined.')
-    } else if (players.playerOne && !players.playerTwo) {
-        players.playerTwo = req.session.id
-        req.session.player = 'Player two'
-        res.json({
-            player: 2,
-        })
-        console.log('Player Two has joined.')
-    }
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
